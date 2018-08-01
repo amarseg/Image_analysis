97244c954620a4fd2fae6a43918b1e6914b84576
amarseg <liamarseg@gmail.com>
2018-08-01 17:11:35 +0100

    adding labels getting there

--- explore_test_data.R
+++ explore_test_data.R
@@ -2,12 +2,12 @@ library('tidyverse')
 source('functions_CPA.R')
 
 plate1_test <- 
-  load_CPA_data('../Plate1_20f/test_table','../Plate1_20f/image_well.csv') %>%
+  load_CPA_data('../Plate1_20f/test_table','../Plate1_20f/image_well.csv', plate_n = 1) %>%
   filter(Image_Metadata_QCFlag == 0 & Type == 'DAPI.tif')
 
 p <- ggplot(plate1_test, aes(x = Well, y = Nuclei_AR_Solidity_Filtered_AreaShape_Area))
 p + geom_boxplot()
 
-p <- ggplot(all, aes(x = Well))
+p <- ggplot(plate1_test, aes(x = Well))
 p + geom_bar()
 
--- functions_CPA.R
+++ functions_CPA.R
@@ -8,12 +8,12 @@ load_CPA_data <- function(per_object_data, per_image_data, plate_n)
     separate(Image_FileName_DNA, into = c('Well','Well_n','Picture','Z_axis','Time','Type'), sep = '--') %>%
     transform(Well_n = str_sub(Well_n, start = 5, end = 7))
   
-  strain_data <- read_csv('../Imaging_code/library_strains.csv') %>%
-    select('Ver5.0 position','Systematic ID') %>%
-    separate('Ver5.0 position', into = c('Version','Plate','Well')) %>%
-    filter(Plate == plate_n)
+ strain_data <- read_csv('../Imaging_code/library_strains.csv') %>%
+   select('Ver5.0 position','Systematic ID') %>%
+   separate('Ver5.0 position', into = c('Version','Plate','Well')) %>%
+   filter(Plate == plate_n)
   
   all_data <- left_join(object_data, well_data, by = c('ImageNumber' = 'ImageNumber'))
   all_data_strain <- left_join(all_data, strain_data, by = c('Well_n' = 'Well'))
-  return(all_data)
+  return(all_data_strain)
 }
