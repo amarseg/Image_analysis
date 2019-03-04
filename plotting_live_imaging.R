4868ce2420d69212c7532a38413c98d7d17603c3
amarseg <liamarseg@gmail.com>
2019-03-04 16:58:56 +0000

    new plots

--- plotting_live_imaging.R
+++ plotting_live_imaging.R
@@ -10,17 +10,41 @@ ggplot(all_data, aes(x = Solidity)) +
   geom_histogram()
 
 as_filter <- all_data %>%
-  filter(aspect_ratio > 1.5 & Solidity > 0.9 & Time < 10) %>%
+  filter(aspect_ratio > 1.5 & Solidity > 0.9) %>%
   group_by(`Systematic ID`,Time) %>%
   summarise(mean_area = mean(Area),
             median_area = median(Area),
             sd_area = sd(Area))
 
-ggplot(as_filter, aes(x = Time, y = Area)) +
-  geom_point() + 
-  geom_line(aes(group = `Systematic ID`)) + 
-  geom_smooth()
+test_gene <- all_data %>%
+  filter((`Systematic ID` == 'SPCC285.17' | `Systematic ID` == 'SPCC18B5.09c') & Time <12)
+
+
+ggplot(test_gene, aes(x = Time, y = log2(Area), group = interaction(Time, `Systematic ID`), color = `Systematic ID`)) +
+  geom_boxplot() 
 
 
 ggplot(all_data, aes(x = TIME/60, y = Area)) +
   geom_hex()
+
+only_wt <- as_filter %>%
+  filter( `Systematic` == 'wt')
+
+ggplot(only_wt, aes(x = Time, y = median_area)) +
+  geom_point() + 
+  geom_line() + 
+  geom_smooth()
+
+
+###################Add data from previous screening##################
+hits <- read_csv('z_score_hits.csv') %>%
+  select(`Systematic ID`,hits, size)
+
+all_data_annotated <- left_join(all_data, hits, by = 'Systematic ID') %>%
+  write_csv('all_data_live_imaging_annotated.csv')
+
+all_data_annotated[all_data_annotated$`Systematic ID`=='wt',]$size <- 'wt'
+
+ggplot(subset(all_data_annotated, Time < 12), aes(x = Time, y = log2(Area))) +
+  geom_boxplot(aes(group = Time))+
+  facet_wrap(~size)
--- processing_live_imaging.R
+++ processing_live_imaging.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(gtools)
 
 data_path <- 'Z:/Pers_Amalia/Screening/live_imaging/processed_data/plate1'
 file_names <- list.files(data_path, recursive = T, full.names = T) %>%
@@ -17,6 +18,13 @@ join_data2 <- file_names2 %>%
   map(read_csv) %>%
   reduce(bind_rows) %>%
   add_column(plate = 'plate2')
+
+join_all_data <- bind_rows(join_data, join_data2) %>%
+  select(-X1) %>%
+  separate(Image_name, into = c('Well','Well_n','Position','Z','Time','File'), sep = '--') %>%
+  select(-File) %>%
+  mutate_at(vars(Well_n:Time), str_sub,start = 4, end = 6) %>%
+  mutate_at(vars(Well_n:Time), as.numeric)
 
 #########Load time data############
 time_stamp <- read_delim('AcquisitionLog.dat', delim = '\t', skip = 1, col_names = c('IMAGEPOS',
@@ -42,3 +50,39 @@ time_stamp2<- apply(time_stamp2, c(1,2), as.numeric) %>%
 join_time_stamp <- bind_rows(time_stamp, time_stamp2)
 
 ################Load strain data#################
+
+strain_data <- read_csv('out.csv') %>%
+  select(`Systematic ID`,`96_well`) %>%
+  add_column(plate = 'plate1') 
+
+strain_data <- strain_data[mixedorder(strain_data$`96_well`),] %>%
+  rownames_to_column()
+
+strain_data2 <- read_csv('out2.csv') %>%
+  select(`Systematic ID`,`96_well`) %>%
+  add_column(plate = 'plate2')
+  
+strain_data2 <- strain_data2[mixedorder(strain_data2$`96_well`),] %>%
+  rownames_to_column()
+
+strain_data$rowname <- as.numeric(strain_data$rowname)
+strain_data2$rowname <- as.numeric(strain_data2$rowname)
+
+join_strain_data <- bind_rows(strain_data, strain_data2)
+
+####################Join everything together#############################
+
+strain_time <- right_join(join_strain_data, join_time_stamp, by = c('plate', 'rowname' = 'W') )
+
+all_data <- left_join(join_all_data, strain_time, by = c('Well_n'='rowname',
+                                                         'Position' = 'P',
+                                                         'Time' = 'T',
+                                                         'plate')) %>%
+  mutate(aspect_ratio = Major_axis_length/Minor_axis_length)
+
+all_data[rowSums(is.na(all_data))>1,]$`Systematic ID` <- 'wt'
+write_csv(all_data, 'all_live_imaging_data.csv')
+
+
+
+
